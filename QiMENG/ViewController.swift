e670f56c3e1603e4371652af749b393551710ff1
QiMeng <616684889@qq.com>
2014-08-30 19:37:56 +0800

    创建项目

---
+++ QiMENG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiMENG
+//
+//  Created by QiMENG on 14-8-30.
+//  Copyright (c) 2014年 QiMeng_LYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
